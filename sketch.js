2cfe59dbeffb5edfc75a87d9f61b383480367808
Hyun S Kim <hyunkims@gmail.com>
2016-01-30 18:57:25 -0800

    add animation along circle

--- sketch.js
+++ sketch.js
@@ -1,14 +1,54 @@
-// Define any global variables here
-// (BUT don't call any p5 methods here;
-//  call them in the functions below!)
+var rot = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // Use the full browser window
-  // Additional setup goes here
+  //background(0);
 }
 
 function draw() {
-  // Put your drawing code here
+  var x, y, r, pt, diff;
+
+  stroke("#D3F3C8");
+  noFill();
+
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+
+  r = 200;
+
+  // big circle
+  drawCircle(x, y, r);
+
+  pt = {};
+
+  pt = getOrbitPos(x, y, r, rot);
+
+
+  stroke("#FA7A55");
+
+
+  drawCircle(pt.x, pt.y, 5);
+
+  rot += PI / 80;
 }
 
-// Define any additional helper functions here
+function drawCircle(x, y, r) {
+  var diam = r * 2;
+  ellipse(x, y, diam, diam);
+}
+
+function getOrbitPos(x, y, r, rad){
+  var pt = {};
+
+  rad -= PI/2;
+
+  pt.x = cos(rad) * r + x;
+  pt.y = sin(rad) * r + y;
+
+  return pt;
+}
+
+function drawOrbit(x, y, r, rad){
+
+}
+

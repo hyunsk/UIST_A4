6a3751e91cb53c044d70e6ef6446e05d6b56b79d
Hyun S Kim <hyunkims@gmail.com>
2016-02-06 21:05:50 -0800

    do refactor

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-var planetBaseSpeed = 0;
 var orbitPathColors = [];
 var univ = [];
 var stars = [];
 var starMaxRadius = null;
 var orbitScaleFactor = 1;
 var orbiterSizeScaleFactor = 1;
+var planetBaseSpeed = null;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -14,7 +14,7 @@ var orbiterSizeScaleFactor = 1;
 function setup() {
   createCanvas(windowWidth, windowHeight); // Use the full browser window
   createUniverse();
-  frameRate(60);
+
 }
 
 //
@@ -24,24 +24,54 @@ function setup() {
 function createUniverse(){
   var kick, snare, hat, synth, bass;
   var tempOrbiter;
+  var x = windowWidth / 2;
+  var y = windowHeight / 2;
 
-  planetBaseSpeed = PI / 200;
-
+  planetBaseSpeed = PI  / 200;
 
   // create planet kick
   // first ring
-  kick = createOrbiter(0, planetBaseSpeed, 8, 150, "#106EE8", 1, 10, 10);
+  kick = createOrbiter({
+    initRotation: 0,
+    delta: 1,
+    radius: 8,
+    rotationRadius: 150,
+    color: "#106EE8",
+    flareDecay: 10
+  });
   // create planet kick's moons
   kick.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 2.2, 2, 20, "#0FC1A1", 1)
+    createOrbiter({
+      initRotation: 0,
+      delta: 2.2,
+      radius: 2,
+      rotationRadius: 20,
+      color: "#0FC1A1",
+      flareDecay: 1
+    })
   );
   kick.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 1.8, 3, 25, "#90E0AB", 1)
+    createOrbiter({
+      initRotation: 0,
+      delta: 1.8,
+      radius: 3,
+      rotationRadius: 25,
+      color: "#90E0AB",
+      flareDecay: 1
+    })
   );
 
   kick.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 1, 1, 32, "#CBFFCE", 1)
+    createOrbiter({
+      initRotation: 0,
+      delta: 1,
+      radius: 1,
+      rotationRadius: 32,
+      color: "#CBFFCE",
+      flareDecay: 1
+    })
   );
+
   // set planet kick orbit color
   orbitPathColors.push("#E5FCC2");
   // add planet kick to univ
@@ -50,13 +80,35 @@ function createUniverse(){
 
   // create planet hat
   // second ring
-  hat = createOrbiter(PI/2, planetBaseSpeed, 12, 270, "#37B7B5", 1, 20, 10);
+  hat = createOrbiter({
+    initRotation: PI/2,
+    delta: 1,
+    radius: 12,
+    rotationRadius: 270,
+    color: "#37B7B5",
+    flareDecay: 10
+  });
+
   // create planet kick's moons
   hat.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 3, 3, 30, "#A0E4E0", 1)
+    createOrbiter({
+      initRotation: 0,
+      delta: 3,
+      radius: 3,
+      rotationRadius: 30,
+      color: "#A0E4E0",
+      flareDecay: 0
+    })
   );
   hat.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 2, 5, 45, "#C7F6F5", 1)
+    createOrbiter({
+      initRotation: 0,
+      delta: 2,
+      radius: 5,
+      rotationRadius: 45,
+      color: "#C7F6F5",
+      flareDecay: 0
+    })
   );
   // set planet kick orbit color
   orbitPathColors.push("#9DE0AD");
@@ -66,10 +118,24 @@ function createUniverse(){
 
   // create planet snare
   // third ring
-  snare = createOrbiter(PI, planetBaseSpeed * 1.4, 16, 380, "#59A27A", 10, 20, 10);
+  snare = createOrbiter({
+    initRotation: PI,
+    delta: 1.4,
+    radius: 16,
+    rotationRadius: 380,
+    color: "#59A27A",
+    flareDecay: 10
+  });
   // create planet snare's moons
   snare.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 5, 4, 40, "#FACADE", 1)
+    createOrbiter({
+      initRotation: 0,
+      delta: 5,
+      radius: 4,
+      rotationRadius: 40,
+      color: "#FACADE",
+      flareDecay: 0
+    })
   );
 
   // set planet snare's orbit color
@@ -80,16 +146,47 @@ function createUniverse(){
 
   // create planet synth
   // fourth ring
-  synth = createOrbiter(3*PI/2, planetBaseSpeed * .5, 30, 560, "#A56CC1", 1, 40, 35);
+  synth = createOrbiter({
+    initRotation: 3*PI/2,
+    delta: 0.5,
+    radius: 30,
+    rotationRadius: 560,
+    color: "#A56CC1",
+    flareDecay: 35
+  });
+
   // create planet synth's moons
-  tempOrbiter = createOrbiter(3*PI/2, planetBaseSpeed * 2, 6, 50, "#A6ACEC", 3);
-  tempOrbiter.orbiters.push(createOrbiter(3*PI/2, planetBaseSpeed * 4, 8, 100, "#ACE7EF", 2));
+  tempOrbiter = createOrbiter({
+    initRotation: 3*PI/2,
+    delta: 2,
+    radius: 6,
+    rotationRadius: 50,
+    color: "#A6ACEC",
+    flareDecay: 0
+  });
+
+  tempOrbiter.orbiters.push(
+    createOrbiter({
+      initRotation: 3*PI/2,
+      delta: 4,
+      radius: 8,
+      rotationRadius: 100,
+      color: "#ACE7EF",
+      flareDecay: 0
+    })
+  );
+
+  synth.orbiters.push(tempOrbiter);
 
   synth.orbiters.push(
-    tempOrbiter
-  );
-  synth.orbiters.push(
-    createOrbiter(0, planetBaseSpeed * 2.5, 12, 120, "#ACE7EF", 5)
+    createOrbiter({
+      initRotation: 0,
+      delta: 2.5,
+      radius: 12,
+      rotationRadius: 120,
+      color: "#ACE7EF",
+      flareDecay: 0
+    })
   );
   // set planet synth orbit color
   orbitPathColors.push("#547980");
@@ -98,23 +195,53 @@ function createUniverse(){
 
   // create planet bass
   // fifth ring
-  bass = createOrbiter(3*PI/2, planetBaseSpeed * 0.2, 40, 760, "#E14242", 1, 50, 40);
+  bass = createOrbiter({
+      initRotation: 3*PI/2,
+      delta: 0.2,
+      radius: 40,
+      rotationRadius: 760,
+      color: "#E14242",
+      flareDecay: 40
+  });
+
   // create planet bass' moons
-  tempOrbiter = createOrbiter(3*PI/2, planetBaseSpeed * 1.2, 10, 150, "#EACD65", 3);
-  tempOrbiter.orbiters.push(createOrbiter(3*PI/2, planetBaseSpeed * 4, 6, 40, "#8D3434", 2));
+  tempOrbiter = createOrbiter({
+    initRotation: 3*PI/2,
+    delta: 1.2,
+    radius: 10,
+    rotationRadius: 150,
+    color: "#EACD65",
+    flareDecay: 0
+  });
+
+  tempOrbiter.orbiters.push(
+    createOrbiter({
+      initRotation: 3*PI/2,
+      delta: 4,
+      radius: 6,
+      rotationRadius: 40,
+      color: "#8D3434",
+      flareDecay: 0
+    })
+  );
 
   bass.orbiters.push(
     tempOrbiter
   );
   bass.orbiters.push(
-    createOrbiter(0, planetBaseSpeed, 10, 80, "#EBEBCD", 5)
+    createOrbiter({
+      initRotation: 0,
+      delta: 1,
+      radius: 10,
+      rotationRadius: 80,
+      color: "#EBEBCD",
+      flareDecay: 0
+    })
   );
   // set planet bass' orbit color
   orbitPathColors.push("#2D727F");
   // add planet bass to univ
   univ.push(bass);
-
-
 
   generateStars(500, 1000, 4);
 
@@ -187,23 +314,23 @@ function draw() {
 
 
     // trigger flare
-    //doFlare = isAtOrbitRotation(PI/2, planet);
+    doFlare = isAtOrbitRotation(PI/2, planet);
 
-    doFlare = false;      // remove flares for now
+    //doFlare = false;      // remove flares for now
 
     // draw planet
-    drawOrbiter(x, y, planet, doFlare);
-    updateOrbiterRotation(planet);
+    planet.draw(x, y, doFlare);
+    planet.updateRotation();
 
     // draw planet's orbiters
-    drawOrbiters(planet.x, planet.y, planet.orbiters);
+    planet.drawOrbiters(planet.x, planet.y, false);
   }
 
 
 
 
 
-
+  frameRate();
 
 }
 
@@ -231,156 +358,136 @@ function drawStar(star){
 
 }
 
-function createOrbiter(initRotation, delta, radius, rotationRadius, strokeColor, strokeWeight, flareDecay, flareMaxLength){
+function createOrbiter(options){
   // initRotation - where to begin
-  // rotation - current rotation around parent
   // delta - how fast to orbit
   // radius - radius of orbiter
+  // rotation - current rotation around parent
   // rotationRadius - distance to center of rotation
-  // strokeColor(string) - stroke color of orbiter
+  // strokeColor (string or color obj) - stroke color of orbiter
   // strokeWeight - stroke weight of orbiter
+  // flareDecay - number of particles emitted before death
+  // orbitCenter - [x, y]
 
+  var orbiter, defaults, override, orbiterColor;
 
-  // scale orbiter's size and rotation radius by global scale factors
-  radius *= orbiterSizeScaleFactor;
-  rotationRadius *= orbitScaleFactor;
+  orbiterColor = color(options.color);
 
-  var orbiter = {orbiters: []};
-  orbiter.initRotation = initRotation;
-  orbiter.rotation = initRotation;
-  orbiter.delta = delta;
-  orbiter.radius = radius;
-  orbiter.initRadius = radius;
-  orbiter.rotationRadius = rotationRadius;
-  orbiter.strokeColor = color(strokeColor);
-  orbiter.initStrokeColor = orbiter.strokeColor;
-  orbiter.strokeWeight = strokeWeight;
-
-  // initial values for x, y
-  orbiter.x = 0;
-  orbiter.y = 0;
-
-  // flare animation properties
-  orbiter.doFlare = false;
-  orbiter.flareDecay = 0;
-  orbiter.flareMaxLength = 0;
-  orbiter.flareLength = 0;
-  orbiter.flareAge = 0;
-
-  if(typeof flareDecay !== "undefined"){
-    orbiter.flareDecay = flareDecay;
+  orbiter = {};
+  defaults = {
+    rotation: options.initRotation,
+    initRadius: options.radius,
+    flareAge: 0,
+    flareDecay: 0,
+    doFlare: false,
+    particles: new ParticleSystem(createVector(orbiter.x, orbiter.y)),
+    orbiters: [],
+    x: 0,
+    y: 0
+  };
+  override = {
+    radius: options.radius * orbiterSizeScaleFactor,        // scale orbiter's size and rotation radius by global scale factors
+    rotationRadius: options.rotationRadius * orbitScaleFactor,
+    delta: planetBaseSpeed * options.delta,
+    color: orbiterColor,
+    initColor: orbiterColor
   }
+  _.assign(orbiter, defaults, options, override);
 
-  if(typeof flareMaxLength !== "undefined"){
-    orbiter.flareMaxLength = flareMaxLength;
-  }
+  orbiter.drawOrbiters = function(x, y, doFlare){
+    var i, currentOrbiter;
+    for (i = 0; i< orbiter.orbiters.length; i++){
+      currentOrbiter = orbiter.orbiters[i];
+      currentOrbiter.draw(x, y, doFlare);
+      currentOrbiter.updateRotation();
 
-  orbiter.particles = new ParticleSystem(createVector(orbiter.x, orbiter.y));
+      if (currentOrbiter.orbiters.length){
+        currentOrbiter.drawOrbiters(currentOrbiter.x, currentOrbiter.y, false)
+      }
+    }
+  };
+
+  orbiter.draw = function(x, y, doFlare){
+    var pt;
+
+    if (doFlare){
+      orbiter.doFlare = true;
+    }
+
+    if (orbiter.doFlare){
+      orbiter.drawFlare(x, y);
+    }
+
+    fill(orbiter.color);
+    noStroke();
+
+    pt = getOrbitPos(x, y, orbiter.rotationRadius, orbiter.rotation);
+
+    orbiter.x = pt.x;
+    orbiter.y = pt.y;
+
+    drawCircle(orbiter.x, orbiter.y, orbiter.radius);
+  };
+
+  orbiter.drawFlare = function(x, y){
+    var direction, dx, dy;
+
+    orbiter.initRotation = orbiter.rotation;
+    orbiter.initColor = orbiter.color;
+
+    if (orbiter.flareAge >= orbiter.flareDecay){
+      // reset flare
+
+      orbiter.doFlare = false;
+      orbiter.flareAge = 0;
+      orbiter.radius = orbiter.initRadius;
+
+      while(orbiter.particles.particles.length){
+        delete orbiter.particles.particles.pop()
+      }
+      return;
+    }
+
+    orbiter.particles.origin.x = orbiter.x;
+    orbiter.particles.origin.y = orbiter.y;
+
+    // calculate slope from orbiter to orbit origin
+    dx = x - orbiter.particles.origin.x;
+    dy = y - orbiter.particles.origin.y;
+
+    // scale slope
+    if (abs(dx) > abs(dy)){     // first scale to [-1, 1]
+      dy /= abs(dx);
+      dx /= abs(dx);
+    }else{
+      dx /= abs(dy)
+      dy /= abs(dy);
+    }
+
+    // scale to map function
+    dx = map(dx, -1, 1, -0.1, 0.1);
+    dy = map(dy, -1, 1, -0.1, 0.1);
+
+    // negative inverse for tangent
+    direction = createVector(-dy, dx);
+
+    orbiter.particles.addParticle(direction, orbiter.color);
+
+    orbiter.particles.run();
+
+
+    orbiter.rotation = orbiter.initRotation;
+    orbiter.color = orbiter.initColor;
+    orbiter.doFlare = true;
+
+    orbiter.flareAge++;
+  };
+
+  orbiter.updateRotation = function(){
+    orbiter.rotation += orbiter.delta;
+  };
 
   return orbiter;
-}
-
-function drawOrbiters(x, y, univ, doFlare){
-  var i, orbiter;
-  for (i = 0; i< univ.length; i++){
-    orbiter = univ[i];
-    drawOrbiter(x, y, orbiter, doFlare);
-    updateOrbiterRotation(orbiter);
-
-    if (orbiter.orbiters.length){
-      drawOrbiters(orbiter.x, orbiter.y, orbiter.orbiters)
-    }
-  }
-}
-
-function drawOrbiter(x, y, orbiter, doFlare){
-  var pt;
-
-  if (doFlare){
-    orbiter.doFlare = true;
-  }
-
-  if (orbiter.doFlare){
-    drawOrbiterFlare(x, y, orbiter);
-  }
-
-  fill(orbiter.strokeColor);
-  noStroke();
-
-  pt = getOrbitPos(x, y, orbiter.rotationRadius, orbiter.rotation);
-
-  orbiter.x = pt.x;
-  orbiter.y = pt.y;
-
-  drawCircle(orbiter.x, orbiter.y, orbiter.radius);
-
-}
-
-function updateOrbiterRotation(orbiter){
-  orbiter.rotation += orbiter.delta;
-}
-
-function drawOrbiterFlare(x, y, orbiter){
-  var i, j, alphaDelta, currentAlpha, lifeRatio, radiusMultiplier, flareParticles, pt, direction, dx, dy;
-
-  orbiter.initRotation = orbiter.rotation;
-  orbiter.initStrokeColor = orbiter.strokeColor;
-
-  if (orbiter.flareAge >= orbiter.flareDecay){
-    // reset flare
-
-    orbiter.doFlare = false;
-    orbiter.flareAge = 0;
-    orbiter.flareLength = 0;
-    orbiter.radius = orbiter.initRadius;
-
-    while(orbiter.particles.particles.length){
-      delete orbiter.particles.particles.pop()
-    }
-    return;
-  }
-
-  lifeRatio = orbiter.flareAge / orbiter.flareDecay;
-
-  alphaDelta = -1 / orbiter.flareLength;
-
-
-  orbiter.particles.origin.x = orbiter.x;
-  orbiter.particles.origin.y = orbiter.y;
-
-  // calculate slope from orbiter to orbit origin
-  dx = x - orbiter.particles.origin.x;
-  dy = y - orbiter.particles.origin.y;
-
-  // scale slope
-  if (abs(dx) > abs(dy)){     // first scale to [-1, 1]
-    dy /= abs(dx);
-    dx /= abs(dx);
-  }else{
-    dx /= abs(dy)
-    dy /= abs(dy);
-  }
-
-  // scale to map function
-  dx = map(dx, -1, 1, -0.1, 0.1);
-  dy = map(dy, -1, 1, -0.1, 0.1);
-
-  // negative inverse for tangent
-  direction = createVector(-dy, dx);
-
-  orbiter.particles.addParticle(direction, orbiter.strokeColor);
-
-  orbiter.particles.run();
-
-
-  orbiter.rotation = orbiter.initRotation;
-  orbiter.strokeColor = orbiter.initStrokeColor;
-  orbiter.doFlare = true;
-
-  orbiter.flareAge++;
-  if (orbiter.flareLength < orbiter.flareMaxLength){
-    orbiter.flareLength++;
-  }
 }
 
 function drawCircle(x, y, r) {

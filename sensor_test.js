bca5e999b3ddde8d25a7d3fbaebe3ddb4b2d8638
James <jpai@D-10-157-157-123.dhcp4.washington.edu>
2016-02-20 15:28:22 -0800

    added Z rotation stuff to map out the notes

--- sensor_test.js
+++ sensor_test.js
@@ -1,8 +1,13 @@
 var SOCKET_URL = 'wss://fierce-plains-17880.herokuapp.com/';
 var TEAM_NAME  = 'gastropub';
 var socket;
+var baseRotZ = null;
+var setBaseRot = false;
+var previousAngle = -1;
+var angleLimits = [];  // stores Z rotation angle limits in this array
+var angleRangeInvert = []; // stores booleans of whether the range contains 359-1 break
 
-var local_id = 1;
+var local_id = 2;
 
 
 var network = {
@@ -30,7 +35,7 @@ var network = {
     })
   },
   sendDebug: function(msg){
-    socket.emit("debug", local_id, msg)
+    socket.emit("debug", local_id, msg);
   }
 }
 
@@ -39,11 +44,24 @@ function setup(){
 }
 
 function draw() {
+    // checks if rotation is initialized, then sets base Z rotation once
+    if (rotationZ != 0 && setBaseRot == false)
+    {
+      baseRotZ = rotationZ;
+      network.sendDebug("baseRotZ = " + baseRotZ)
+      setupAngles();
+      setBaseRot = true;
+    }
 
+    // sets each new note only once
+    if (mapDeviceAngle() != previousAngle) {
+        network.sendDebug(mapDeviceAngle());  
+        previousAngle = mapDeviceAngle();
+    }
 }
 
 function deviceShaken(){
-  network.sendDebug("hello");
+ // network.sendDebug("hello");
 }
 
 function deviceOrientationIsAtPlane(plane){
@@ -67,9 +85,124 @@ function setupDeviceShaken(){
 
 }
 
-function mapDeviceAngle(){
-  // reads device angle and outputs 0-4 for note
+function setupAngles() {
+  var intervalAmount = 40; // set size of interval angle range
+  var intervalMultiplier = 2.5; // used to calculate the interval values
 
+  // set up interval values and stores in array
+  for (var i = 0; i < 6; i++)
+  {
+      angleLimits[i] = (baseRotZ - 2.5*intervalAmount) + i * intervalAmount;
+      if (angleLimits[i] < 0) {
+        angleLimits[i] = angleLimits[i] + 360;
+      }
+      if (angleLimits[i] > 360) {
+        angleLimits[i] = angleLimits[i] - 360;
+      }
+  }
+
+  // tests to see if any ranges contain the 359-1 break, true if it does
+  for (var i = 0; i < 5; i++)
+  {
+    if (angleLimits[i] > angleLimits[i+1])
+    {
+      angleRangeInvert[i] = true;
+    }
+    else
+    {
+      angleRangeInvert[i] = false;
+    }
+  }
+
+  // console logs
+  for (var i = 0; i < 6; i++)
+  {
+    network.sendDebug(angleLimits[i]);
+  }
+  for (var i = 0; i < 5; i++)
+  {
+    network.sendDebug(angleRangeInvert[i]);
+  }
+}
+
+function mapDeviceAngle(){
+// reads angle ranges and compares current rotation to the range limits
+// outputs a note
+
+  if (!angleRangeInvert[0])
+  {
+    if  (rotationZ > angleLimits[0] && rotationZ < angleLimits[1])
+    {
+      return 4;
+    }
+  }
+  else if (angleRangeInvert[0])
+  {
+    if (rotationZ > angleLimits[0] || rotationZ < angleLimits[1])
+    {
+      return 4;
+    }
+  }
+
+  if (!angleRangeInvert[1])
+  {
+    if  (rotationZ > angleLimits[1] && rotationZ < angleLimits[2])
+    {
+      return 3;
+    }
+  }
+  else if (angleRangeInvert[1])
+  {
+    if (rotationZ > angleLimits[1] || rotationZ < angleLimits[2])
+    {
+      return 3;
+    }
+  }
+
+  if (!angleRangeInvert[2])
+  {
+    if  (rotationZ > angleLimits[2] && rotationZ < angleLimits[3])
+    {
+      return 2;
+    }
+  }
+  else if (angleRangeInvert[2])
+  {
+    if (rotationZ > angleLimits[2] || rotationZ < angleLimits[3])
+    {
+      return 2;
+    }
+  }
+
+  if (!angleRangeInvert[3])
+  {
+    if  (rotationZ > angleLimits[3] && rotationZ < angleLimits[4])
+    {
+      return 1;
+    }
+  }
+  else if (angleRangeInvert[3])
+  {
+    if (rotationZ > angleLimits[3] || rotationZ < angleLimits[4])
+    {
+      return 1;
+    }
+  }
+
+  if (!angleRangeInvert[4])
+  {
+    if  (rotationZ > angleLimits[4] && rotationZ < angleLimits[5])
+    {
+      return 0;
+    }
+  }
+  else if (angleRangeInvert[4])
+  {
+    if (rotationZ > angleLimits[4] || rotationZ < angleLimits[5])
+    {
+      return 0;
+    }
+  }
 }
 
 function mapDeviceAcceleration(axis){
